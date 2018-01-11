cc53f611aa05ff582e6542dca593112baf963bcd
RKIV <robertkellerIV@gmail.com>
2018-01-11 16:57:49 -0500

    Finaling field graph

--- MatPlotLib.py
+++ MatPlotLib.py
@@ -1,9 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
 
-
-field = plt.imread('Field.png')
-fig, ax = plt.subplots()
-ax.imshow(field, extent = [0, 648, 0 , 320])
-ax.grid(color='black', which='both', linestyle='-', linewidth=.1)
+#Making the field plot
+field = plt.imread('Field.png') #get image
+fig, ax = plt.subplots() #make window
+ax.set_xticks(np.arange(0, 648, 48)) #customize tick intervals
+ax.set_yticks(np.arange(0, 320, 48))
+ax.yaxis.set_minor_locator(AutoMinorLocator(6)) #customize minor tick intervals
+ax.xaxis.set_minor_locator(AutoMinorLocator(6))
+ax.grid(color='black', which='both', linestyle='-', linewidth=.05) #add grid
 plt.show()
+#minor ticks are every 18" and major ticks are every 72"
--- SimulationEngine.py
+++ SimulationEngine.py
@@ -5,6 +5,8 @@ from matplotlib.ticker import AutoMinorLocator
 
 cubeDict = {'BPortal0': 5, 'BPortal1' : 6, 'BPyramid' : 10, 'BLine' : 6, 'RPortal0' : 5, 'RPortal1' : 6, 'RPyramid' : 10, 'RLine' : 6}
 objDict = {'ScaleB': 0, 'ScaleR' : 0, 'BSwitchB' : 0, 'BSwitchR' : 0, 'RSwitchB' : 0, 'RSwitchR' : 0} #For Switches first 'R' of 'B' is side of field, second 'R' or 'B' is side of switch 
+mainStartingLocationsB = [[640, 80], [640, 160], [640, 240]]
+mainStartingLocationsR = [[0, 80], [0, 160], [0, 240]]
 gameLength = 10 #seconds
 
 
@@ -25,7 +27,10 @@ class Robot:
     self.switchSpeed = switchSpeed
     self.pickUpSpeed = pickUpSpeed
     self.hasCube = hasCube
-    self.location = [np.random.randint(0,10), np.random.randint(0,10)]
+    if RID[0] == 0:
+      self.location = mainStartingLocationsB[RID[1]]
+    else:
+      self.location = mainStartingLocationsR[RID[1]]
     if RID[0] == 0:
       self.name = self.name + 'B'
     elif RID[0] == 1:
@@ -59,13 +64,13 @@ def Strategy0(robot): #returns list [d, theta, ObjChange, CubeChange]
 #          robotlocation[i] = robotlocation[i] + vector[i]  
 #  else:
 #    print("I have a cube")
-  coordinateChange = [10, math.pi]
+  coordinateChange = [0, math.pi]
   objChange = 'ScaleR'
   cubeChange = ''
   fieldChange = [objChange, cubeChange]
   return coordinateChange, fieldChange
 
-    
+#Defining all of the robots and a list of the robots  
 B0 = Robot(RID = [0, 0])
 B1 = Robot(RID = [0, 1])
 B2 = Robot(RID = [0, 2])
@@ -73,28 +78,35 @@ R0 = Robot(RID = [1, 0])
 R1 = Robot(RID = [1, 1])
 R2 = Robot(RID = [1, 2])
 RobotList = [B0, B1, B2, R0, R1, R2]
-field = plt.imread('Field.png')
-fig, ax = plt.subplots()
-ax.yaxis.set_minor_locator(AutoMinorLocator(5))
-ax.xaxis.set_minor_locator(AutoMinorLocator(5))
-ax.imshow(field, extent = [0, 648, 0 , 320])
-ax.grid(color='black', which='both', linestyle='-', linewidth=.1)
+
+#Making the field plot
+field = plt.imread('Field.png') #get image
+fig, ax = plt.subplots() #make window
+ax.imshow(field, extent = [0, 648, 0 , 320]) #show image with dimensions 648 x 320
+ax.set_xticks(np.arange(0, 648, 48)) #customize tick intervals
+ax.set_yticks(np.arange(0, 320, 48))
+ax.yaxis.set_minor_locator(AutoMinorLocator(6)) #customize minor tick intervals
+ax.xaxis.set_minor_locator(AutoMinorLocator(6))
+ax.grid(color='black', which='both', linestyle='-', linewidth=.05) #add grid
+#minor ticks are every 18" and major ticks are every 72"
 
 if __name__ == "__main__": # Start Here
-	for time in range(gameLength): #The code that runs each second
-		for robot in RobotList: 
-			vector, fieldChange = Strategy0(robot)
-			coordinates = convertToRect(vector[0], vector[1])
-			print(coordinates)
-			for index in range(len(robot.location)): #Changes robot location
-				robot.location[index] = robot.location[index] + coordinates[index]
-			if fieldChange[0] != '': #Changes field element dictionaries
-				objDict[fieldChange[0]] += 1
-			if fieldChange[1] != '':
-				cubeDict[fieldChange[1]]
-	plt.show()
-
-	print('Done')
+  for time in range(gameLength): #The code that runs each second
+    for robot in RobotList:
+      vector, fieldChange = Strategy0(robot) #get the projected movement vector and projected changes to field elements
+      coordinates = convertToRect(vector[0], vector[1]) #covert polar vector to rectangular
+      print(coordinates) #print for debugging
+      for index in range(len(robot.location)): #Changes robot location
+        robot.location[index] = robot.location[index] + coordinates[index]
+      if fieldChange[0] != '': #Changes field element dictionaries
+        objDict[fieldChange[0]] += 1
+      if fieldChange[1] != '':
+        cubeDict[fieldChange[1]]
+    print([robot.location for robot in RobotList])
+    ax.scatter([robot.location[0] + 4 for robot in RobotList[3:]], [robot.location[1] + 4 for robot in RobotList[3:]], s = 3, c = 'red')
+    ax.scatter([robot.location[0] + 4 for robot in RobotList[:3]], [robot.location[1] + 4 for robot in RobotList[:3]], s = 3, c = 'blue')
+    plt.show()
+  print('Done')
     
 
 
